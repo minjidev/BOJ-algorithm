0ae0512d0acf6fa01ec575a44306cfa489ffb4f4
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-03-13 22:06:15 +0900

    [Bronze IV] Title: 곱셈, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,8 +1,7 @@
 a = int(input())
 b = input()
 
-three = a * int(b[2]) 
-four = a * int(b[1]) 
-five = a * int(b[0]) 
-six = three + four * 10 + five * 100
-print(three, four, five, six, sep='\n')
+print(a * int(b[2]))
+print(a * int(b[1]))
+print(a * int(b[0]))
+print(a * int(b))
