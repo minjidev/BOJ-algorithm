ecdfb4e01d5185bf47d66f83097c21e06ad4a3d7
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-03-15 11:53:48 +0900

    [Bronze III] Title: A+B - 7, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -3,4 +3,4 @@ import sys
 t = int(sys.stdin.readline().rstrip())
 for i in range(t):
   sum_val = sum(map(int, sys.stdin.readline().rstrip().split()))
-  print(f"Case #{i+1}: {sum_val}")
+  print(f"Case #%d:"%(i+1), sum_val)
