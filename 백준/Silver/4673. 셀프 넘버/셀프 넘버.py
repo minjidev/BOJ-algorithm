e39c79b0499894471ee698dee446c96718da1dc7
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-03-24 01:33:44 +0900

    [Silver V] Title: 셀프 넘버, Time: 76 ms, Memory: 31372 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,11 +1,11 @@
-n = set(range(1, 10001))
-g = set()
+def d(n):
+  n = n + sum(map(int, str(n)))
+  return n
 
+generated_num = set()
 for i in range(1, 10001):
-  for j in str(i):
-    i += int(j)
-  g.add(i)
+  generated_num.add(d(i))
 
-result = sorted(n - g)
-for i in result:
-  print(i)
+for j in range(1, 10001):
+  if j not in generated_num:
+    print(j)
