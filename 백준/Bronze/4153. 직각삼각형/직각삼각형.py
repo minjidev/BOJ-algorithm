6320efe01184aaf815330ae488cd1970f3b2be0d
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-04-07 00:14:10 +0900

    [Bronze III] Title: 직각삼각형, Time: 68 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -1,12 +1,10 @@
 a=b=c=1
-long=0
 while True:
   sum=0
   lst = []
   a,b,c = map(int, input().split())
   lst.extend([a, b, c])
-  long = max(a,b,c)
-  lst.pop(lst.index(long))
+  long = lst.pop(lst.index(max(a,b,c)))
   for i in lst:
     sum += i**2
 
