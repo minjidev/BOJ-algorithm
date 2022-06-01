73356b94b6b7eb5570fbc7db4e4dddfe315435aa
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-06-01 23:06:28 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,5 +1,14 @@
-word = input()
-alphabets = list(range(97, 123))
+import sys 
+s = sys.stdin.readline().rstrip()
+asn = []
+asc = ord('a')
 
-for i in alphabets:
-  print(word.find(chr(i)))
+for _ in range(26):
+  char = chr(asc)
+  if char in s:
+    asn.append(s.index(char))
+  else:
+    asn.append(-1)
+  asc += 1
+
+print(*asn)
