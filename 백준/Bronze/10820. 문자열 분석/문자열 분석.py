a9375d60ab72959e3b02671464caa9cb2c12b88e
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-06-02 13:30:34 +0900

    [Bronze II] Title: 문자열 분석, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10820. 문자열 분석/문자열 분석.py
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.py
@@ -1,22 +1,18 @@
 import sys 
-input = sys.stdin.readline
-upper='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-lower='abcdefghijklmnopqrstuvwxyz'
-num='0123456789'
-
 
 while True:
-  s = input()
+  s = sys.stdin.readline()
   if s=='':
     break
   u = l = n = wh = 0
   for ch in s:
-    if ch in upper:
+    if ch.isupper():
       u += 1
-    elif ch in lower:
+    elif ch.islower():
       l += 1
-    elif ch in num:
+    elif ch.isnumeric():
       n += 1
     elif ch == ' ':
       wh += 1
+    
   print(l, u, n , wh)
