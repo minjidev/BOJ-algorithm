f9797af7971ef73fc13e35e1e38a94fd719765e4
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-06-02 22:38:51 +0900

    [Silver III] Title: 후위 표기식2, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+pst_fix = input().rstrip()
+num_list = [int(input()) for _ in range(n)]
+stack = []
+A = ord('A')
+Z = ord('Z')
+
+for ch in pst_fix:
+  if A<=ord(ch)<=Z:
+    stack.append(num_list[ord(ch)-A])
+  else:
+    b = stack.pop()
+    a = stack.pop()
+    stack.append(eval(str(a)+ch+str(b)))
+
+print(f'{stack[0]:.2f}')
