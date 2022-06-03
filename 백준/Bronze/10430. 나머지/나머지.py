adb7fe56ce32ccefdbc1c90c1d917b34243609fe
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-06-03 22:55:41 +0900

    [Bronze V] Title: 나머지, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.py
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -1,3 +1,6 @@
-a, b, c = map(int, input().split())
-
-print((a+b)%c, ((a%c) + (b%c))%c,(a*b)%c, ((a%c)*(b%c))%c, sep='\n')
+import sys
+a, b, c = map(int, sys.stdin.readline().split())
+print((a+b)%c)
+print((a+b)%c)
+print((a*b)%c)
+print((a*b)%c)
