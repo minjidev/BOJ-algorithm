d5b608d2419c0648296d2e5ceea097e67b97e982
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-06-04 15:52:05 +0900

    [Silver IV] Title: 소수 찾기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,16 +1,18 @@
-import sys
-n = int(sys.stdin.readline())
-num_list = list(map(int, sys.stdin.readline().split()))
-p_cnt = 0
+import sys 
 
+def prime(n):
+  if n<2:
+    return 0
+  for i in range(2, int(n**0.5)+1):
+    if n%i == 0:
+      return 0
+  return 1
+  
+input = sys.stdin.readline
+n = input()
+num_list = [int(i) for i in input().split()]
+ans  = 0
 for num in num_list:
-  if num > 1: 
-    cnt = 0
-    for j in range(2, num):
-      if num%j == 0:
-        cnt += 1
-    if cnt == 0:
-      p_cnt += 1
-      
-
-print(p_cnt)
+  ans += prime(num)
+  
+print(ans)
