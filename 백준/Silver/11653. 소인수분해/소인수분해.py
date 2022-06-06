a785fc82445e30038a467fc4e44ea553e2498824
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-06-07 08:11:45 +0900

    [Silver V] Title: 소인수분해, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11653. 소인수분해/소인수분해.py
+++ 백준/Silver/11653. 소인수분해/소인수분해.py
@@ -1,9 +1,10 @@
 import sys
 n = int(sys.stdin.readline())
-
-if n>1:
-  for i in range(2, n+1):
-    if (n%i)==0:
-      while (n%i)==0:
-        n = n//i
-        print(i)
+# 인수 중 가장 큰 값은 √n 
+for i in range(2, int(n**0.5)+1):
+  while n%i==0:
+    n //= i
+    print(i)
+# 남는 몫 출력 
+if n > 1:
+    print(n)
