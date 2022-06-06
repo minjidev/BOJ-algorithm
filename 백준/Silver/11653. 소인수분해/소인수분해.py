9a5f4dce59bf9364d00c03f1b026f25fff387069
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-06-07 07:28:47 +0900

    [Silver V] Title: 소인수분해, Time: 1040 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11653. 소인수분해/소인수분해.py
+++ 백준/Silver/11653. 소인수분해/소인수분해.py
@@ -1,7 +1,9 @@
-n = int(input())
-if n > 1:
+import sys
+n = int(sys.stdin.readline())
+
+if n>1:
   for i in range(2, n+1):
-    if n%i == 0:
-      while n%i == 0:
+    if (n%i)==0:
+      while (n%i)==0:
+        n = n//i
         print(i)
-        n /= i
