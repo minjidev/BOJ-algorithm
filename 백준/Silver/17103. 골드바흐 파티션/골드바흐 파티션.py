9dc09c0be53370c39e51af8c621ff869fea4bfdd
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-06-06 17:23:39 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 260 ms, Memory: 131096 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -1,24 +1,20 @@
 import sys
-
-
-MAX = 1000001
-check = [True] * (MAX)
-# 에라토스테네스의 체 
-check[0]=check[1]=False
-for i in range(2, (int(MAX**0.5))+1):
-  if check[i]:
-    for j in range(2*i, MAX, i):
-      check[j] = False
-
 input = sys.stdin.readline
 t = int(input())
-for _ in range(t):
-  n = int(input())
-  ans = 0
-  for i in range(n):
-    # 순서만 다른 경우는 세지 말 것 
-    if check[i] and check[n-i]:
-      if i <= n-i:
-        ans+=1 
-  print(ans)
-  
+num_lst =[int(input()) for i in range(t)]
+m = max(num_lst)
+check = [False, False] + [True]*(m-1)
+
+# 입력 숫자 중 최대숫자까지 소수 구하기
+for i in range(2, int(m**0.5)+1):
+  if check[i]:
+    for j in range(2*i, m+1, i):
+      check[j] = False
+
+for num in num_lst:
+  cnt = 0
+  # 순서만 다른 것은 포함 X
+  for i in range((num//2)+1):
+    if check[i] and check[num-i]:
+      cnt += 1
+  print(cnt)
