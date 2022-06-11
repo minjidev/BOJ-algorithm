aeec8392cf5cd3feb859983868f35b81e5dc1090
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-06-12 03:01:10 +0900

    [Silver II] Title: 연속합, Time: 128 ms, Memory: 38548 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+a = list(map(int, input().split()))
+d = [0] * n
+d[0] = a[0]
+
+for i in range(n):
+  d[i] = max(d[i-1]+a[i], a[i])
+
+print(max(d))
