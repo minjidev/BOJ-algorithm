cd1426e6f0f25cf6a270390cc057504c57e87162
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-06-12 17:14:20 +0900

    [Silver I] Title: 동물원, Time: 220 ms, Memory: 50052 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -6,12 +6,8 @@ d=[[0]*3 for _  in range(n+1)]
 d[0][0] = 1
 
 for i in range(1, n+1):
-  d[i][0] = d[i-1][0] + d[i-1][1] + d[i-1][2]
-  d[i][1] = d[i-1][0] + d[i-1][2]
-  d[i][2] = d[i-1][0] + d[i-1][1]
-
-  d[i][0] %= mod
-  d[i][1] %= mod
-  d[i][2] %= mod
-
+  d[i][0] = (d[i-1][0] + d[i-1][1] + d[i-1][2]) % mod
+  d[i][1] = (d[i-1][0] + d[i-1][2]) % mod
+  d[i][2] = (d[i-1][0] + d[i-1][1]) % mod
+  
 print(sum(d[n])%mod)
