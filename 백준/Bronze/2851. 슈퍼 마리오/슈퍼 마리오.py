2fc0e1a035fa6b0a96f86bf37f01ce0cf5058dc3
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-06-25 13:04:02 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
@@ -2,21 +2,14 @@ import sys
 input = sys.stdin.readline
 mushrooms = [int(input()) for i in range(10)]
 mush_sum = [sum(mushrooms[:i+1]) for i in range(10)]
-ans = 0
 
-if mush_sum[-1] < 100: # 합이 100미만이면 전체 합이 정답
-  ans = mush_sum[-1]
-  
+diff, min_idx = 100, 0
+
 for i in range(10):
-  if mush_sum[i] == 100: # 합이 100이면 반복문 탈출
-    ans = mush_sum[i]
-    break
-  elif mush_sum[i] > 100:
-    # 100이하 값이 더 가까울 때 
-    if abs(100-mush_sum[i-1])<abs(100-mush_sum[i]): 
-      ans = mush_sum[i-1]
-    else:
-      ans = mush_sum[i]
+  if abs(100-mush_sum[i]) <= diff: # 100과 더 가깝거나 같은 값인 경우
+    diff = abs(100-mush_sum[i]) # 100과 가장 작은 차이 저장
+    min_idx = i # 100과 가장 차이가 작게 나는 index 저장
+  else:
     break
 
-print(ans)
+print(mush_sum[min_idx]) # 100과 차이가 가장 작을 때 합계값 출력
