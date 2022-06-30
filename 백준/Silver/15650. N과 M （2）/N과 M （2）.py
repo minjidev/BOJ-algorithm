9ca91ad678628a3fa0546e7ddbbd011edf371d0f
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-06-30 16:19:58 +0900

    [Silver III] Title: N과 M (2), Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -2,15 +2,17 @@ import sys
 n,m = map(int, sys.stdin.readline().split())
 arr = [0] * m
 
-def p_asc(index, start, n, m):
-  if index == m:
+
+def p_asc(num, selected, n, m): # 자연수, 선택한 수의 개수
+  if selected == m:
     print(*arr)
-    return 
+    return
+  if num>n:
+    return
 
-  for i in range(start, n+1):  
-    arr[index] = i # index자리에 i(start~N)
-    p_asc(index+1, i+1, n, m) # index+1 자리에는 i+1부터 가능 
+  arr[selected] = num # 해당 수 포함하는 경우 
+  p_asc(num+1, selected+1, n, m)
+  arr[selected] = 0 # 해당 수 포함하지 않는 경우 
+  p_asc(num+1, selected, n, m)
 
-p_asc(0, 1, n, m)
-
-
+p_asc(1, 0, n, m)
