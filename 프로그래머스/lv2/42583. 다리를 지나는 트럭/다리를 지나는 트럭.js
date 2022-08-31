f30648fc902689bd670537c60b7886b712206d70
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-08-31 18:51:55 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.14 ms, Memory: 30.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,4 +1,23 @@
 function solution(bridge_length, weight, truck_weights) {
-    var answer = 0;
-    return answer;
+    let bridge = new Array(bridge_length).fill(0), 
+        bridge_weight = 0, 
+        res = 0
+       
+    
+    while (bridge.length) {
+        bridge_weight -= bridge.shift()
+        // 대기 트럭 있으면 무게 제한 확인해서 올리기 
+        if (truck_weights.length) {
+            let truck = truck_weights[0]
+            if (bridge_weight+truck<=weight) {
+                bridge_weight += truck
+                bridge.push(truck_weights.shift())
+            } else {
+                bridge.push(0)
+            }
+            
+        }
+        res += 1 
+    }
+    return res
 }
