2a3f64696611d6321d8cb6c2f9051a2d4a58f0b3
minji99710 <68722909+minji99710@users.noreply.github.com>
2022-09-01 10:13:07 +0900

    [Gold IV] Title: 오큰수, Time: 1048 ms, Memory: 265472 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+function sol(n, arr) {
+  res = new Array(n).fill(-1);
+
+  stack = [];
+  for (let i = 0; i < arr.length; i++) {
+    let cur = arr[i];
+
+    while (stack.length && stack.at(-1)[1] < cur) {
+      res[stack.pop()[0]] = cur;
+    }
+    stack.push([i, cur]);
+  }
+  return res.join(" ");
+}
+
+let arr = input[1].split(" ").map((item) => +item);
+console.log(sol(+input[0], arr));
