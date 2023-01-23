3c5c4161dd7961b1d7e4482bb877fe8c8e9f04c2
minji99710 <68722909+minji99710@users.noreply.github.com>
2023-01-23 11:50:30 -0400

    [level 1] Title: 신규 아이디 추천, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,10 +1,11 @@
 function solution(new_id) {
-    let createdId = new_id.toLowerCase().replace(/[^a-z0-9-_.]/g, '');
-  createdId = createdId.replace(/[.]{2,}/g, '.');
-  createdId = createdId.replace(/^[.]|[.]$/g, '');
+  let createdId = new_id
+    .toLowerCase()
+    .replace(/[^a-z0-9-_.]/g, '')
+    .replace(/[.]{2,}/g, '.')
+    .replace(/^[.]|[.]$/g, '');
   if (createdId.length === 0) createdId = 'a';
-  if (createdId.length >= 16) createdId = createdId.substring(0, 15);
-  createdId = createdId.replace(/[.]$/, '');
+  if (createdId.length >= 16) createdId = createdId.substring(0, 15).replace(/[.]$/, '');
   if (createdId.length <= 2) {
     createdId += createdId.slice(-1).repeat(3 - createdId.length);
   }
