df98cda84f16d0f014762f30e4dd342639278c23
minjidev <68722909+minjidev@users.noreply.github.com>
2023-05-22 08:05:27 +0900

    [Silver I] Title: 미로 탐색, Time: 200 ms, Memory: 11936 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,43 +1,38 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "prac.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
-let [n, m] = input.shift().split(" ").map(Number);
-const board = input.map((v) => v.split("").map(Number));
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const [N, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const q = [];
-let answer = 0;
+const [n, m] = N.split(" ").map(Number);
+let board = arr.map((row) => row.split("").map(Number));
 
-let dis = Array.from(Array(n), () => Array(m).fill(0)); // 거리 배열
-let ch = Array.from(Array(n), () => Array(m).fill(0)); // 방문 확인 배열
-// 출발 위치 넣어주기
-q.push([0, 0]);
-dis[0][0] = 1;
-ch[0][0] = 1;
+function BFS(n, m, board) {
+  const dx = [-1, 0, 1, 0];
+  const dy = [0, 1, 0, -1];
+  let q = [];
+  let dis = Array.from({ length: n + 1 }, () => Array(m + 1).fill(0));
 
-const dx = [-1, 0, 1, 0];
-const dy = [0, 1, 0, -1];
+  q.push([0, 0]);
+  dis[0][0] = 1;
+  board[0][0] = 0;
 
-function BFS(x, y) {
   while (q.length) {
-    let [x, y] = q.shift();
-    for (let k = 0; k < 4; k++) {
-      let nx = x + dx[k];
-      let ny = y + dy[k];
-      if (
-        nx >= 0 &&
-        nx < n &&
-        ny >= 0 &&
-        ny < m && 
-        board[nx][ny] === 1 &&
-        ch[nx][ny] === 0
-      ) {
-        ch[nx][ny] = 1;
-        dis[nx][ny] = dis[x][y] + 1;
+    const [x, y] = q.shift();
+
+    for (let i = 0; i < dx.length; i++) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+
+      if (nx === n - 1 && ny === m - 1) {
+        return dis[x][y] + 1;
+      }
+
+      if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] === 1) {
         q.push([nx, ny]);
+        board[nx][ny] = 0;
+        dis[nx][ny] = dis[x][y] + 1;
       }
     }
   }
-  return dis[n - 1][m - 1];
 }
-console.log(BFS(0, 0));
 
+console.log(BFS(n, m, board));
