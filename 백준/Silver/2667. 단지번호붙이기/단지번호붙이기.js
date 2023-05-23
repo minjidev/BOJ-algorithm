59072adef94c6171eb34bc37b6b24c443b4695ca
minjidev <68722909+minjidev@users.noreply.github.com>
2023-05-23 09:40:15 +0900

    [Silver I] Title: 단지번호붙이기, Time: 144 ms, Memory: 9528 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -1,51 +1,66 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "prac.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
-let n = Number(input.shift());
-const board = input.map((v) => v.split("").map(Number));
-// console.log("board: ", board);
-let cntArr = [];
-let ch = Array.from(Array(n), () => Array(n).fill(0)); // 방문 확인 배열
-// 매번 새로 돌아야하므로 내부에 cnt 선언하기
-function BFS(x, y) {
-  let cnt = 1;
-  const dx = [-1, 0, 1, 0];
-  const dy = [0, 1, 0, -1];
-
-  queue = [];
-  queue.push([x, y]);
-
-  ch[x][y] = 1;
-  while (queue.length) {
-    let [x, y] = queue.shift();
-    for (let k = 0; k < 4; k++) {
-      let nx = x + dx[k];
-      let ny = y + dy[k];
-      if (
-        nx >= 0 &&
-        nx < n &&
-        ny >= 0 &&
-        ny < n &&
-        board[nx][ny] === 1 &&
-        ch[nx][ny] === 0
-      ) {
-        ch[nx][ny] = 1;
-        cnt++;
-        queue.push([nx, ny]);
-      }
-    }
+class Queue {
+  constructor() {
+    this.data = [];
+    this.head = 0;
+    this.tail = 0;
   }
-  cntArr.push(cnt);
+  push(item) {
+    this.data[this.tail++] = item;
+  }
+  pop() {
+    this.head++;
+  }
+  front() {
+    return this.data[this.head];
+  }
+  rear() {
+    return this.data[this.tail - 1];
+  }
+  isEmpty() {
+    return this.head === this.tail;
+  }
+  size() {
+    return Math.abs(this.head - this.tail);
+  }
 }
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const [N, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const n = +N;
+const board = arr.map((row) => row.split("").map(Number));
+const dx = [-1, 0, 1, 0];
+const dy = [0, 1, 0, -1];
+const answer = [];
 
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < n; j++) {
-    if (board[i][j] === 1 && ch[i][j] === 0) {
-      BFS(i, j);
+    if (board[i][j] === 1) {
+      const q = new Queue();
+      q.push([i, j]);
+      board[i][j] = 0;
+      let cnt = 1;
+
+      while (!q.isEmpty()) {
+        const [x, y] = q.front();
+        q.pop();
+
+        for (let i = 0; i < dx.length; i++) {
+          const nx = x + dx[i];
+          const ny = y + dy[i];
+
+          if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+            board[nx][ny] = 0;
+            q.push([nx, ny]);
+            cnt += 1;
+          }
+        }
+      }
+
+      answer.push(cnt);
     }
   }
 }
-
-cntArr.sort((a, b) => a - b);
-console.log(cntArr.length);
-cntArr.forEach((x) => console.log(x));
+answer.sort((a, b) => a - b);
+console.log([answer.length, ...answer].join("\n"));
