56c4f219c7e44bb4667608cda9392bd7314287f3
minjidev <68722909+minjidev@users.noreply.github.com>
2023-06-03 17:32:36 +0900

    [Silver V] Title: 큰 수 구성하기, Time: 180 ms, Memory: 9880 KB -BaekjoonHub

--- 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.js
+++ 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.js
@@ -1,38 +1,23 @@
+const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = require("fs")
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split("\n");
+let [n, arr] = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let [N, K] = input[0].split(" ").map((el) => +el);
-const set = input[1].split(" ").map((el) => +el);
-const arr = [];
+const [N, K] = n.split(" ").map(Number);
+const nums = arr.split(" ").map(Number);
+nums.sort((a, b) => a - b);
 let answer = 0;
 
-for (let i = 1; i <= String(N).length; i++) dfs(0, i);
-
-console.log(answer);
-
-function dfs(depth, length) {
-  if (depth === length) {
-    let cand = 0;
-
-    for (const num of arr) {
-      cand += num;
-      cand *= 10;
+// 재귀 근데 몇 번 내려가야할지 안 정해져있음
+function DFS(L, num) {
+  // 숫자가 완성되면
+  if (num > N) return;
+  else {
+    for (let i = 0; i < K; i++) {
+      answer = Math.max(num, answer);
+      DFS(L + 1, 10 * num + nums[i]);
     }
-    cand /= 10;
-
-    if (N >= cand) answer = Math.max(answer, cand);
-
-    return;
-  }
-
-  // 순열
-  for (let i = 0; i < K; i++) {
-    arr.push(set[i]);
-    dfs(depth + 1, length);
-    arr.pop();
   }
 }
+
+DFS(0, 0);
+console.log(answer);
