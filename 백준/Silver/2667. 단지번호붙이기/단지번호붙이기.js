d692400f87983836c24114b56a35df5396767773
minjidev <68722909+minjidev@users.noreply.github.com>
2023-08-14 15:21:05 +0900

    [Silver I] Title: 단지번호붙이기, Time: 124 ms, Memory: 9524 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -23,44 +23,55 @@ class Queue {
     return Math.abs(this.head - this.tail);
   }
 }
-
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const [N, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
+let [n, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const n = +N;
+const N = Number(n);
 const board = arr.map((row) => row.split("").map(Number));
-const dx = [-1, 0, 1, 0];
-const dy = [0, 1, 0, -1];
-const answer = [];
+const dir = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+];
 
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < n; j++) {
+let answer = [];
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N; j++) {
     if (board[i][j] === 1) {
-      const q = new Queue();
-      q.push([i, j]);
       board[i][j] = 0;
-      let cnt = 1;
-
-      while (!q.isEmpty()) {
-        const [x, y] = q.front();
-        q.pop();
-
-        for (let i = 0; i < dx.length; i++) {
-          const nx = x + dx[i];
-          const ny = y + dy[i];
-
-          if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
-            board[nx][ny] = 0;
-            q.push([nx, ny]);
-            cnt += 1;
-          }
-        }
-      }
-
-      answer.push(cnt);
+      const count = BFS(i, j);
+      answer.push(count);
     }
   }
 }
-answer.sort((a, b) => a - b);
-console.log([answer.length, ...answer].join("\n"));
+
+function BFS(x, y) {
+  const queue = new Queue();
+  let count = 1;
+  queue.push([x, y]);
+
+  while (!queue.isEmpty()) {
+    const [x, y] = queue.front();
+    queue.pop();
+
+    for (let k = 0; k < 4; k++) {
+      const nx = x + dir[k][0];
+      const ny = y + dir[k][1];
+
+      if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+      if (board[nx][ny] === 0) continue;
+
+      board[nx][ny] = 0;
+      count += 1;
+      queue.push([nx, ny]);
+    }
+  }
+
+  return count;
+}
+
+console.log(answer.length);
+console.log(answer.sort((a, b) => a - b).join("\n"));
