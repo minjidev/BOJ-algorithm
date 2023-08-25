813255e10d91a869591794d978df21fe561e4c75
minjidev <68722909+minjidev@users.noreply.github.com>
2023-08-25 15:29:53 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,22 +1,9 @@
 function solution(s) {
-    const map = {
-        'zero': 0,
-        'one': 1,
-        'two': 2,
-        'three': 3,
-        'four': 4,
-        'five': 5,
-        'six': 6,
-        'seven': 7,
-        'eight': 8,
-        'nine': 9,
-    }
+    const nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     
-   
-    for (let key of Object.keys(map)) {
-        if (s.includes(key)) {
-            s = s.replaceAll(key, map[key])
-        }
+    for (let i=0;i<nums.length;i++) {
+        s = s.split(nums[i])
+        s = s.join(i)
     }
     
     return Number(s)
