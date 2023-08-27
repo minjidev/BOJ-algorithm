364afd7fcd9827b62ca4e12ea83a5ac31110cf6e
minjidev <68722909+minjidev@users.noreply.github.com>
2023-08-27 14:10:33 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.17 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,14 +1,15 @@
 function solution(new_id) {
-  let createdId = new_id
-    .toLowerCase()
-    .replace(/[^a-z0-9-_.]/g, '')
-    .replace(/[.]{2,}/g, '.')
-    .replace(/^[.]|[.]$/g, '');
-  if (createdId.length === 0) createdId = 'a';
-  if (createdId.length >= 16) createdId = createdId.substring(0, 15).replace(/[.]$/, '');
-  if (createdId.length <= 2) {
-    createdId += createdId.slice(-1).repeat(3 - createdId.length);
-  }
+    let recommendedId = new_id
+      .toLowerCase()
+      .replace(/[^a-z0-9-_\.]/g, "")
+      .replace(/\.{2,}/g, ".")
+      .replace(/^\.|\.$/g, "");
 
-  return createdId;
+    if (!recommendedId) recommendedId = "a";
+
+    const len = recommendedId.length;
+    if (len >= 16) recommendedId = recommendedId.slice(0, 15).replace(/\.$/g, "");
+    if (len <= 2) recommendedId += recommendedId.at(-1).repeat(3 - len);
+
+    return recommendedId
 }
