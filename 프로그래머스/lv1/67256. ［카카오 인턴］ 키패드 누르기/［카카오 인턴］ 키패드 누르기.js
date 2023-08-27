d1cb55b5a4f94e893a87cabb51a95db034c34e9a
minjidev <68722909+minjidev@users.noreply.github.com>
2023-08-27 13:22:14 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.63 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,38 +1,31 @@
 function solution(numbers, hand) {
-    let result = ''
-    const hands = [0, 'L', 0, 'R', 'L', 0, 'R', 'L', 0, 'R']
-    const curPos = {'L': '*', 'R': '#'}
-    const middle = {2: [3, 1, 0, 1, 2, 1, 2, 3, 2, 3], 
-                   5: [2, 2, 1, 2, 1, 0, 1, 2, 1, 2],
-                   8: [1, 3, 2, 3, 2, 1, 2, 1, 0, 1],
-                   0: [0, 4, 3, 4, 3, 2, 3, 2, 1, 2]}
-    
+    // 세로는 아래에서부터 [0, 1, 2, 3], 가로는 [1, 0, 1]
+    const position = [0, 3, 3, 3, 2, 2, 2, 1, 1, 1]
+    const curPos = {'L': [0, 1], 'R': [0, 1]}
     const key = hand === 'right' ? 'R' : 'L'
     
-    for (let num of numbers) {
-        // 양쪽 
-        if (hands[num]) {
-            curPos[hands[num]] = num // 이동 
-            result += hands[num]
-        } else {
-            // 중앙 : 거리 계산 -> 같으면 손 확인 
-            const leftDis = isNaN(curPos['L']) ? middle[num][0] + 1 : middle[num][curPos['L']]
-            const rightDis = isNaN(curPos['R']) ? middle[num][0] + 1 : middle[num][curPos['R']]
-            
-            // 오른손이 더 가까우면
-            if (rightDis < leftDis) {
-                curPos['R'] = num
-                result += 'R'
-            } else if (leftDis < rightDis) {
-                // 왼손이 더 가까우면 
-                curPos['L'] = num
-                result += 'L'
-            } else {
-                curPos[key] = num
-                result += key
-            }
+    return numbers.map(num => {
+        if (/[147]/.test(num)) {
+            curPos['L'] = [position[num], 1]
+            return 'L'
         }
-    }
-    
-    return result
+        if (/[369]/.test(num)) {
+            curPos['R'] = [position[num], 1]
+            return 'R'
+        }
+        
+        const disL = Math.abs(position[num] - curPos['L'][0]) + curPos['L'][1]
+        const disR = Math.abs(position[num] - curPos['R'][0]) + curPos['R'][1]
+        if (disL === disR) {
+            curPos[key] = [position[num], 0]
+            return key
+        }
+        
+        if (disL < disR) {
+            curPos['L'] = [position[num], 0]
+            return 'L'
+        }
+        curPos['R'] = [position[num], 0]
+        return 'R'
+    }).join('')
 }
