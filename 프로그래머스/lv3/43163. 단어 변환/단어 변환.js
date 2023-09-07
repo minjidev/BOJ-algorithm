6846ed1f4ffe352f08f5d9301e0f71afc3889993
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-07 17:57:31 +0900

    [level 3] Title: 단어 변환, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -1,30 +1,58 @@
-function solution(begin, target, words) {
-    // 문자열 한 개 다른지 확인
-    function compareStr(str1, str2) {
-        let diff = [...str1].reduce((acc, cur, idx)=> {
-            if (cur!==str2[idx]) {
-                acc++
-            }
-            return acc
-        }, 0)
-        return diff===1
+class Queue {
+    constructor() {
+        this.head = 0
+        this.tail = 0
+        this.data = []
     }
-    // BFS
-    let q = []
-    let visited = []
+    push(item) {
+        this.data[this.tail++] = item
+    }
+    pop() {
+        this.head++
+    }
+    front() {
+        return this.data[this.head]
+    }
+    rear() {
+        return this.data[this.tail - 1]
+    }
+    isEmpty() {
+        return this.head === this.tail
+    }
+    size() {
+        return Math.abs(this.head - this.tail)
+    }
+}
+
+function solution(begin, target, words) {    
+    const ch = {}
+    const queue = new Queue()
+    ch[begin] = 1
+    queue.push([begin, 0]) // [단어, 변경 횟수]
     
-    q.push([begin, 0])
-    if (!words.includes(target)) return 0
     
-    while (q.length) {
-        const [curStr, cnt] = q.shift()
-        if (curStr===target) return cnt
-        for (let i=0;i<words.length;i++) {
-            if(compareStr(curStr, words[i]) && !visited[words[i]]) {
-                q.push([words[i], cnt+1])
-                visited.push(words[i]) // 방문 표시
+    function isNext(current, next) {
+        // current가 next로 넘어갈 수 있는지, 즉 1글자 차이인지 확인 
+        let cnt = 0
+        const len = current.length
+        for (let i=0;i<len;i++) {
+            if (current[i] !== next[i]) cnt +=1 
+        }
+        return cnt === 1
+    }
+    
+    while (!queue.isEmpty()) {
+        const [curw, count] = queue.front()
+        queue.pop()
+        
+        for (let nw of words) {
+            if (ch[nw]) continue
+            if (isNext(curw, nw)) {
+                if (nw === target) return count + 1
+                ch[nw] = 1
+                queue.push([nw, count + 1])
             }
         }
-        
     }
+    return 0
 }
