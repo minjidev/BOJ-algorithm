7afa2be3c3e04a62218db87a16ce293a1a0638db
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-07 17:00:32 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.js
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -1,23 +1,23 @@
 function solution(n, computers) {
-    let ch = Array.from({length: n}, () => false)
+    const ch = Array(n).fill(0)
     let cnt = 0
     
-    function DFS(i) {
-        ch[i] = true 
-        // 다음 노드가 방문한 적 없고, 연결되어 있는 경우 
-        for (let j=0;j<computers[i].length;j++) {
-            if (!ch[j] && computers[i][j]===1) {
-                DFS(j)
+    function DFS(L) {
+        ch[L] = 1
+        
+        for (let i=0;i<n;i++) {
+            if (ch[i] === 0 && computers[L][i] === 1) {
+                DFS(i)
             }
         }
     }
     
-    for (let i=0;i<computers.length;i++) {
-        if (!ch[i]) {
-            DFS(i)
-            cnt++
-        }
+    for (let i=0;i<n;i++) {
+        if (ch[i] === 1) continue
+        
+        DFS(i)
+        cnt += 1
     }
     
-    return cnt
+    return cnt 
 }
