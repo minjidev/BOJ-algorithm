08ed8637917034c931f3afecf1c34218f1516fae
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-08 23:05:31 +0900

    [level 3] Title: N으로 표현, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.js
@@ -1,25 +1,35 @@
-function solution(N, number) {
-    const possibleNums = Array.from({length:9}, ()=>new Set())
-    // N을 1~8번 사용해 number 만들 수 있는지 확인 
-    for (let i=1;i<9;i++) {
-        // 사칙연산 없이 N을 i개 사용
-        possibleNums[i].add(Number(String(N).repeat(i)))
-        for (let j=1;j<i;j++) {
-            for (let el1 of possibleNums[j]) {
-                for (let el2 of possibleNums[i-j]) {
-                    // 더해서 i가 되는 횟수들의 배열을 사칙연산 
-                    possibleNums[i].add(el1+el2)
-                    possibleNums[i].add(el1-el2)
-                    possibleNums[i].add(el1*el2)
-                    possibleNums[i].add(Math.floor(el1/el2))
+const calFunc = [(a, b) => a + b, (a, b) => a - b, (a, b) => a * b, (a, b) => Math.floor(a / b)]
+
+function getPossibleNums(cnt, dp, N) {
+    let result = new Set() // 만들 수 있는 숫자 
+    result.add(+String(N).repeat(cnt))
+    
+    for (let i=1;i<cnt;i++) {
+        for (let func of calFunc) {
+            for (const item1 of dp[i].values()) {
+                for (const item2 of dp[cnt-i].values()) {
+                    result.add(func(item1, item2))
                 }
+                
             }
         }
-        // i번 사용했을 때 number를 만든 경우
-        if (possibleNums[i].has(number)) {
-            return i
-        }
     }
-    // N을 1~8번 사용했는데 number을 만들지 못한 경우 
+    
+    return result
+}
+
+function solution(N, number) {
+    if (N === number) return 1 // 현재 숫자인 경우
+    
+    // dp[i] : 숫자 N을 i번 사용해서 만들 수 있는 숫자
+    const dp = [] 
+    dp[1] = new Set([N]) // 1번 사용은 그냥 그 숫자
+    
+    for (let i=2;i<=8;i++) {
+        dp[i] = getPossibleNums(i, dp, N) // 사용 개수, dp배열, 숫자
+        
+        if (dp[i].has(number)) return i // 타겟 숫자 찾으면 return 
+    }
+    
     return -1
 }
