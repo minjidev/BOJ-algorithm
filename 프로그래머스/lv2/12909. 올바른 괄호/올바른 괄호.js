56c6805a0acbc43dc5f06c0485f1ae3399bb0e50
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-09 19:17:47 +0900

    [level 2] Title: 올바른 괄호, Time: 4.82 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,14 +1,10 @@
 function solution(s){
-    s = s.split('')
-    let stack = []
-    if (s[0]===')') return false
-    
-    for (let i=0;i<s.length;i++) {
-        if (s[i]==='(') stack.push(0)
-        else {
-            if(s.length) stack.pop()
-        }
+    // 개수가 맞고, 시작이 (인 경우 올바른 괄호
+    let sum = 0
+    for (let ch of s) {
+        sum += ch === '(' ? 1 : -1
+        if (sum < 0) return false
     }
-
-    return stack.length===0 ? true : false
+    if (sum === 0) return true
+    return false
 }
