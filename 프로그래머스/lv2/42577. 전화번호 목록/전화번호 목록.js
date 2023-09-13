78190f4824171b41a8bc1e1f404e6b60f2ea57d5
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-13 18:02:22 +0900

    [level 2] Title: 전화번호 목록, Time: 161.60 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
@@ -3,7 +3,7 @@ function solution(phone_book) {
     
     for (let i=1;i<phone_book.length;i++) { 
         // 현재 값이 다음 값의 접두어인지 확인
-        if (phone_book[i].indexOf(phone_book[i-1]) === 0) return false
+        if (phone_book[i].startsWith(phone_book[i-1])) return false
     }
     return true 
 }
