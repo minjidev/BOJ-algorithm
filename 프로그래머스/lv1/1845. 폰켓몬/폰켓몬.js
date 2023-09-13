bc0ed6320fafd113cffa3bf688ff84cb8577431c
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-13 16:34:54 +0900

    [level 1] Title: 폰켓몬, Time: 0.22 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,11 +1,7 @@
 function solution(nums) {
-    let count = nums.reduce((obj, cur) => {
-        if (!obj[cur]) {
-            obj[cur] = 0
-        }
-        obj[cur] += 1
-        return obj
-    }, {})
-   
-    return Math.min(Object.keys(count).length, nums.length/2)
+    // 종류(Set.size)와 nums.length/2 를 비교
+    // 종류가 더 많으면 길이 리턴, 아니면 종류 리턴. 같으면 아무거나 리턴
+    const types = new Set(nums).size
+    const len = nums.length/2
+    return len <= types ? len : types
 }
