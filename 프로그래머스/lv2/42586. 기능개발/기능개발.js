632ae2e14832ada9cd95cee7fdc25a7e14a805a3
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-15 21:27:24 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,20 +1,19 @@
 function solution(progresses, speeds) {
-    // 각각 걸리는 시간을 저장하고, queue에 저장.
-    // queue에서 뒤의 날짜가 걸리는 시간이 작거나 같으면 같이 배포, 아니면 따로 배포 시작
-    const queue = progresses.map((prog, i) => Math.ceil((100 - progresses[i]) / speeds[i]))
-    let maxVal = queue[0]
-    let cnt = 1
-    const answer = []
-    
-    for (let i=1;i<queue.length;i++) {
-        if (queue[i] <= maxVal) {
-            cnt += 1
-        } else {
-            answer.push(cnt)
-            maxVal = queue[i]
-            cnt = 1
-        }
-    }
+   const leftDays = progresses.map((p, i) => Math.ceil((100 - p) / speeds[i]))
+   const answer = []
+   let maxVal = leftDays[0]
+   let cnt = 0
+   
+   for (let day of leftDays) {
+       if (maxVal < day) {
+           maxVal = day
+           answer.push(cnt)
+           cnt = 1
+       } else {
+           cnt += 1
+       }
+   }
     answer.push(cnt)
+    
     return answer
 }
