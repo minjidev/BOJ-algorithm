f517532cefcaef67cb25b15cc9de6a7acad7b971
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-16 10:23:17 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,31 +1,31 @@
 function solution(bridge_length, weight, truck_weights) {
     /** 
-    *   bridge_length 길이의 큐 bridge 만들기 
-    *   weight이하 && bridge_length이하까지 큐에 추가 
-    *   아니라면 큐에 0 넣으면서 계속 밀기. 추가할 때는 다리 길이 유지 위해 shift() 
-    *   전체 bridge에 올라간 무게가 0이면 반복문 종료 
+    * bridge_length길이의 배열 bridge 만들기 
+    * 시간이 흐를때마다 
+        - shift() -> 값이 0이 아니면 passed에 push -> 만약 passed.length === truck_weights.length와 같으면 return time 
+        - 만약 truck이 있고, sum + 다음 트럭 무게 <= weight : 다음 트럭 push, 아니면 0 push 
+        - 빼고 넣고 할 때 weightSum에 무게 빼고 더하기 
     */
-    const bridge = Array.from({length: bridge_length}, () => 0)
+    const bridge = Array(bridge_length).fill(0)
+    const passed = []
+    const trucksLen = truck_weights.length
+    let time = 0
+    let weightSum = 0 
     
-    // 첫 번째 트럭 올리기
-    let weightSum = truck_weights[0]
-    let time = 1
-    bridge.push(truck_weights.shift())
-    bridge.shift()
-    
-    while (weightSum > 0) {
-        // 다리 맨 앞을 빼고, 시간이 1초 흐른다. (앞을 빼면서 가능한 경우 바로 다음 트럭을 올려줘야해서 먼저 수행)
-        weightSum -= bridge.shift()
-        time += 1 
-        // 다음 트럭이 다리에 올라갈 수 있다면 
-        if (truck_weights.length > 0 && weightSum + truck_weights[0] <= weight) {
+    while (true) {
+        // 앞을 빼고 
+        time += 1
+        const passedTruck = bridge.shift()
+        weightSum -= passedTruck
+        if (passedTruck > 0) passed.push(passedTruck)
+        if (passed.length === trucksLen) return time
+        
+        // 뒤에 넣기 
+        if (truck_weights.length && weightSum + truck_weights[0] <= weight) {
             weightSum += truck_weights[0]
             bridge.push(truck_weights.shift())
-        } else {
-            // 못 올라간다면 무게 더하지 않기 위해 0 넣어주기 
-            bridge.push(0)
-        }
+        } else bridge.push(0)        
     }
-    return time
     
+
 }
