6aa0c391188b9dca69b939d50af36df8a7fb8b6b
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-18 17:06:10 +0900

    [level 1] Title: 모의고사, Time: 3.74 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,18 +1,17 @@
 function solution(answers) {
-    const s1 = [1, 2, 3, 4, 5],
-          s2 = [2, 1, 2, 3, 2, 4, 2, 5],
-          s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    let scores = [0, 0, 0]
-    answers.forEach((x, idx) => {
-        if (x===s1[idx%s1.length]) scores[0] += 1
-        if (x===s2[idx%s2.length]) scores[1] += 1
-        if (x===s3[idx%s3.length]) scores[2] += 1
-    })
+    const s1 = [1, 2, 3, 4, 5]
+    const s2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    const s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    let scores = Array(4).fill(0)
     
-    let maxVal = Math.max(...scores), result = [];
-    for (let i=0;i<scores.length;i++) {
-        if (maxVal === scores[i]) result.push(i+1)
+    for (let i=0;i<answers.length;i++) {
+        const a = answers[i]
+        scores[1] += (a === s1[i%s1.length])
+        scores[2] += (a === s2[i%s2.length])
+        scores[3] += (a === s3[i%s3.length])
     }
     
-    return result
+    const maxS = Math.max(...scores)
+    const bestS = [1, 2, 3].filter(s => scores[s] === maxS)
+    return bestS
 }
