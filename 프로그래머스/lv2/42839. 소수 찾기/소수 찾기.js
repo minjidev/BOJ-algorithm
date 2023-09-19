7436c2575de5ff2e32946343f9b7d859b8f4072d
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-19 13:05:53 +0900

    [level 2] Title: 소수 찾기, Time: 0.44 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,6 +1,6 @@
 function isPrime(num) {
     if (num <= 1) return false
-    for (let i=2;i<num;i++) {
+    for (let i=2;i<=Math.sqrt(num);i++) {
         if (num % i === 0) return false
     }
     return true
