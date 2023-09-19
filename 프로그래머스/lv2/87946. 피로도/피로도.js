6ea6ab37b095dcf5aa122958397d28e003389a51
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-19 14:27:21 +0900

    [level 2] Title: 피로도, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,38 +1,23 @@
-function getOrder(length) {
-    const arr = Array.from({ length }, (_, idx) => idx)
-    const ch = Array(length).fill(0)
-    const answer = []
-    function DFS(L, order) {
-        if (L === length) answer.push(order)
-        for (let i=0;i<length;i++) {
-            if (ch[i] === 1) continue
+function solution(k, dungeons) {
+    // DFS로 방문할 수 있는 가능한 순서로 돌면서 각 던전 방문할 때 감소한 피로도, cnt 넘겨주기 
+    const N = dungeons.length
+    const ch = Array(N).fill(0)
+    let maxVal = Number.MIN_SAFE_INTEGER
+    
+    function DFS(hp, cnt) {
+        maxVal = Math.max(maxVal, cnt) 
+        
+        for (let i=0;i<N;i++) {
+            const [min, damage] = dungeons[i]
+            
+            if (ch[i] === 1 || hp < min) continue
+            
             ch[i] = 1
-            DFS(L + 1, [...order, arr[i]])
+            DFS(hp - damage, cnt + 1)
             ch[i] = 0
         }
     }
-    DFS(0, [])
-    return answer
-}
-
-
-function solution(k, dungeons) {
-    // 방문할 순서를 순열로 정하기(L===n일때)
-    // 그 순서대로 각 던전에서 if (k>=d[0]) -> k-d[1]하면서 cnt 증가
-    const orders = getOrder(dungeons.length)
-    let maxVal = Number.MIN_SAFE_INTEGER
     
-    for (let order of orders) {
-        let cnt = 0
-        let hp = k
-        for (let o of order) {
-            const [min, damage] = dungeons[o]
-            if (hp < min) break
-            hp -= damage
-            cnt += 1
-        }
-        
-        if (maxVal < cnt) maxVal = cnt
-    }
+    DFS(k, 0)
     return maxVal
 }
