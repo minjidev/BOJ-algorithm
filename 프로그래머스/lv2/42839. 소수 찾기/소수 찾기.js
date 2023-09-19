49ff04cbc4574ec1c059c1884d369b11a5e0e213
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-19 13:02:44 +0900

    [level 2] Title: 소수 찾기, Time: 0.52 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,44 +1,30 @@
-// 순열 구하기
-const getPermutations = function (arr, selectNum) {
-  const result = [];
-  if (selectNum === 1) {
-    return arr.map((val) => [val]);
-  } // 1개 선택할 때, 배열의 각 원소를 return
-
-  arr.forEach((fixed, idx, origin) => {
-    const rest = [...origin.slice(0, idx), ...origin.slice(idx + 1)]; // 현재 고정 숫자 제외 모든 값
-    const permutations = getPermutations(rest, selectNum - 1);
-    const attached = permutations.map((permutation) => [fixed, ...permutation]);
-    result.push(...attached);
-  });
-  return result;
-};
-
-// 소수 구하기
-const checkPrime = function (n) {
-  let sieve = Array.from({ length: n + 1 })
-    .fill(true)
-    .fill(false, 0, 2);
-  for (let i = 2; i * i <= n; i++) {
-    if (sieve[i]) {
-      for (let j = 2 * i; j < n + 1; j += i) {
-        sieve[j] = false;
-      }
+function isPrime(num) {
+    if (num <= 1) return false
+    for (let i=2;i<num;i++) {
+        if (num % i === 0) return false
     }
-  }
-  return sieve;
-};
+    return true
+}
+
+function getPermutations(n) {
+    const len = n.length
+    const ch = Array(len).fill(0)
+    const answer = []
+    
+    function DFS(str) {
+        answer.push(+str)
+        for (let i=0;i<len;i++) {
+            if (ch[i] === 1) continue
+            ch[i] = 1
+            DFS(str + n[i])
+            ch[i] = 0
+        }
+    }
+    DFS('')
+    return answer
+}
 
 function solution(numbers) {
-  // numbers에서 selectNum개 뽑아 순열 만들기
-  let permutations = numbers.split("").reduce((p, cur, idx, arr) => {
-    const each = getPermutations(arr, idx + 1).map((x) => Number(x.join("")));
-    p.push(...each);
-    return p;
-  }, []);
-  permutations = Array.from(new Set(permutations));
-  const maxVal = Math.max(...permutations);
-  const primes = checkPrime(maxVal);
-
-  return permutations.filter((x) => primes[x]).length;
+    // numbers로 가능한 숫자 만들기(순열) -> 소수인 개수 구하기
+    return Array.from(new Set(getPermutations(numbers))).filter(num => isPrime(num)).length
 }
