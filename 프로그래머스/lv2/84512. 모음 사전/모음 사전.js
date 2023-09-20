7d73af3600288964b71860a05110fae39d6d910a
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-20 16:07:15 +0900

    [level 2] Title: 모음 사전, Time: 4.75 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,26 +1,17 @@
 function solution(word) {
-    // 어떻게 DFS 중단하고 값을 반환하지? 변수 cnt에 갱신하고 flag로 탈출 
-    // 중복을 허용하는 순열 
     const alphabets = ['A' ,'E', 'I', 'O', 'U']
+    let result = {}
     let cnt = 0
-    let flag = 0
     
-    function DFS(str) {
-        if (flag) return // 값을 찾으면 중단
-        cnt += 1 // 몇 번째인지 세기
-        if (str === word) {
-            flag = 1
-            return
-        }
-        if (str.length >= 5) return // 5개까지만 탐색
-        
-        for (let alphabet of alphabets) {
-            DFS(str + alphabet)
-        }
+    function DFS(str, length) {
+        if (length > 5) return // 5개까지만
+        result[str] = cnt // 해당 단어 몇 번째인지
+        cnt += 1
+        alphabets.forEach(a => DFS(str + a, length + 1))
     }
-    
-    DFS('')
-    return cnt - 1 // 빈 문자열 센 거 빼기
+
+    DFS('', 0)
+    return result[word]
 }
 
 
