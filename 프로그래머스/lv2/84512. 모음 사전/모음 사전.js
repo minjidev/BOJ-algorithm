a90d3a68929465ff4b0398d958f853492eba528e
minjidev <68722909+minjidev@users.noreply.github.com>
2023-09-20 15:24:52 +0900

    [level 2] Title: 모음 사전, Time: 0.30 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,28 +1,26 @@
-const getPermutations = function (arr, selectNum) {
-  const result = [];
-
-  if (selectNum === 1) return arr.map((val) => [val]);
-
-  arr.forEach((fixed, idx, origin) => {
-    const rest = arr;
-    const permutations = getPermutations(rest, selectNum - 1);
-    const attached = permutations.map((val) => [fixed, ...val]);
-    result.push(...attached);
-  });
-  return result;
-};
-
-
 function solution(word) {
-    const alphabets = ["A", 'E', 'I', 'O', 'U']
-    const dict = alphabets.reduce((dict, val, idx, arr) => {
-        const each = getPermutations(arr, idx+1).map((val)=> val.join(''))
-        dict.push(...each)
-        return dict
-    }, [])
+    // 어떻게 DFS 중단하고 값을 반환하지? 변수 cnt에 갱신하고 flag로 탈출 
+    // 중복을 허용하는 순열 
+    const alphabets = ['A' ,'E', 'I', 'O', 'U']
+    let cnt = 0
+    let flag = 0
     
-    dict.sort()
-    return dict.indexOf(word)+1
+    function DFS(str) {
+        if (flag) return // 값을 찾으면 중단
+        cnt += 1 // 몇 번째인지 세기
+        if (str === word) {
+            flag = 1
+            return
+        }
+        if (str.length >= 5) return // 5개까지만 탐색
+        
+        for (let alphabet of alphabets) {
+            DFS(str + alphabet)
+        }
+    }
+    
+    DFS('')
+    return cnt - 1 // 빈 문자열 센 거 빼기
 }
 
 
