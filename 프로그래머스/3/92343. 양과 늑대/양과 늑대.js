b6c7238013c54a7ac4638c9c1b1d0c859591784e
minjidev <68722909+minjidev@users.noreply.github.com>
2023-11-24 17:54:31 +0900

    [level 3] Title: 양과 늑대, Time: 1.66 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
@@ -7,29 +7,25 @@ function solution(info, edges) {
         graph[from].push(to)
     }
     
-    // 현재 노드, 양, 늑대, 다음 방문할 정점들
-    function DFS(cur, sheep, wolf, nextNodes) {
-        if (info[cur] === 0) {
-            sheep += 1
-            if (maxSheep < sheep) maxSheep = sheep
-        } else {
-            wolf += 1
-        }
+    // 현재 노드, [양, 늑대], 다음 방문할 정점들
+    function DFS(cur, count, nextNodes) {
+        const [s, w] = [...count]
+        const nextCount = info[cur] === 0 ? [s + 1, w] : [s, w + 1]
         
-        // 양이 잡아먹히는 경우
+        const [sheep, wolf] = nextCount
+        if (maxSheep <= sheep) maxSheep = sheep
         if (sheep <= wolf) return 
         
         // 방문할 노드 리스트에 현재 노드의 자식 노드 넣어주기
-        const next = [...nextNodes, ...graph[cur]]
-        next.splice(next.indexOf(cur), 1)
+        const next = [...nextNodes.filter(el => el !== cur), ...graph[cur]]
  
         for (let node of next) {
-            DFS(node, sheep, wolf, next)
+            DFS(node, nextCount, next)
         }
         
     }
     
-    DFS(0, 0, 0, [0])
+    DFS(0, [0, 0], [0])
     return maxSheep
     
 }
