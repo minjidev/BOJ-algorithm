02a0a08a9f6ffccb415a1145942f09498c0eb96a
minjidev <68722909+minjidev@users.noreply.github.com>
2024-01-15 12:09:09 +0900

    [Silver III] Title: 바이러스, Time: 116 ms, Memory: 9440 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,22 +1,25 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-let [N, M, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let [c, p, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const [n, m] = [N, M].map(Number);
-const map = arr.map((row) => row.split(" ").map(Number));
-const ch = Array(n + 1).fill(0);
-const nodes = Array.from({ length: n + 1 }, () => []);
+const [computerCnt, pairCnt] = [c, p].map(Number);
+const pairs = arr.map((row) => row.split(" ").map(Number));
+const graph = Array.from({ length: computerCnt + 1 }, () => []);
+const ch = Array(computerCnt + 1).fill(0);
 let answer = 0;
 
-// 인접 리스트
-for (let [from, to] of map) {
-  nodes[from].push(to);
-  nodes[to].push(from);
+// 연결 그래프
+for (let [s, e] of pairs) {
+  graph[s].push(e);
+  graph[e].push(s);
 }
 
+// DFS
 function DFS(v) {
-  for (let nv of nodes[v]) {
-    if (ch[nv] === 1) continue;
+  for (let i = 0; i < graph[v].length; i++) {
+    const nv = graph[v][i];
+
+    if (ch[nv]) continue;
 
     ch[nv] = 1;
     answer += 1;
