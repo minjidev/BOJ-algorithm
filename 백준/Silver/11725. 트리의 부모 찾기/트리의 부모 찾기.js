3afe98424d2da609890069a633fd5d0c1c9c37a0
minjidev <68722909+minjidev@users.noreply.github.com>
2024-01-16 10:34:26 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 596 ms, Memory: 97960 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
@@ -1,61 +1,30 @@
-class Queue {
-  constructor() {
-    this.data = [];
-    this.head = 0;
-    this.tail = 0;
-  }
-  push(item) {
-    this.data[this.tail++] = item;
-  }
-  pop() {
-    this.head++;
-  }
-  front() {
-    return this.data[this.head];
-  }
-  rear() {
-    return this.data[this.tail - 1];
-  }
-  isEmpty() {
-    return this.head === this.tail;
-  }
-  size() {
-    return Math.abs(this.head - this.tail);
-  }
-}
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let [n, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const N = Number(n);
-const nodes = arr.map((row) => row.split(" ").map(Number));
+const N = +n;
+const pairs = arr.map((row) => row.split(" ").map(Number));
 
 const graph = Array.from({ length: N + 1 }, () => []);
-const ch = Array.from({ length: N + 1 }).fill(0);
-const parents = Array.from({ length: N + 1 }).fill(0);
+const ch = Array(N + 1).fill(0);
+const parents = Array(N + 1).fill(0);
 
-for (let [from, to] of nodes) {
-  graph[from].push(to);
-  graph[to].push(from);
+for (let [s, e] of pairs) {
+  graph[s].push(e);
+  graph[e].push(s);
 }
 
-const queue = new Queue();
-queue.push(1);
-ch[1] = 1;
-
-while (!queue.isEmpty()) {
-  const v = queue.front();
-  queue.pop();
-
+function DFS(v) {
   for (let i = 0; i < graph[v].length; i++) {
     const nv = graph[v][i];
 
-    if (ch[nv] === 0) {
-      ch[nv] = 1;
-      parents[nv] = v;
-      queue.push(nv);
-    }
+    if (ch[nv]) continue;
+
+    ch[nv] = 1;
+    parents[nv] = v;
+    DFS(nv);
   }
 }
 
+DFS(1);
 console.log(parents.slice(2).join("\n"));
