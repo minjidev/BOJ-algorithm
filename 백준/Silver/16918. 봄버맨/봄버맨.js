ef306833399f75009510398a299f0027bb0caef1
minjidev <68722909+minjidev@users.noreply.github.com>
2024-01-18 12:51:36 +0900

    [Silver I] Title: 봄버맨, Time: 304 ms, Memory: 12440 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.js
+++ 백준/Silver/16918. 봄버맨/봄버맨.js
@@ -1,54 +1,68 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const [N, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
-
-const [r, c, n] = N.split(" ").map(Number);
+let [nums, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
+const [R, C, N] = nums.split(" ").map(Number);
 const board = arr.map((row) => row.split(""));
-const dx = [-1, 0, 1, 0];
-const dy = [0, 1, 0, -1];
-let time = 1;
-let timeBoard = Array.from({ length: r }, () => Array(c).fill(0));
+const timerBoard = Array.from({ length: R }, () => Array(C).fill(0));
+const dir = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+];
+let elapsedTime = 0;
 
-// 폭탄 설치 시간 추가
-for (let i = 0; i < r; i++) {
-  for (let j = 0; j < c; j++) {
-    if (board[i][j] === "O") timeBoard[i][j] = 3;
-  }
-}
+function explodeBombs() {
+  if (elapsedTime <= 1) return;
 
-while (time <= n) {
-  for (let i = 0; i < r; i++) {
-    for (let j = 0; j < c; j++) {
-      // 빈 자리에 새로운 폭탄 설치
-      if (time % 2 === 0 && board[i][j] === ".") {
-        board[i][j] = "O";
-        timeBoard[i][j] = time + 3;
-      }
-
-      if (time % 2 === 1 && time === timeBoard[i][j]) {
+  for (let i = 0; i < R; i++) {
+    for (let j = 0; j < C; j++) {
+      if (board[i][j] === "O" && timerBoard[i][j] === elapsedTime) {
+        // 폭발
         board[i][j] = ".";
-        // bfs 돌면서 3초 지난 경우 폭탄 폭발
-        for (let k = 0; k < dx.length; k++) {
-          const nx = i + dx[k];
-          const ny = j + dy[k];
+        for (let k = 0; k < 4; k++) {
+          const nx = i + dir[k][0];
+          const ny = j + dir[k][1];
 
-          if (
-            nx >= 0 &&
-            nx < r &&
-            ny >= 0 &&
-            ny < c &&
-            board[nx][ny] === "O" &&
-            time !== timeBoard[nx][ny]
-          ) {
-            // 자기자신 포함 4방향
-            board[nx][ny] = ".";
-            timeBoard[nx][ny] = 0;
-          }
+          if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
+          if (board[nx][ny] === ".") continue;
+          if (timerBoard[nx][ny] === elapsedTime) continue;
+
+          board[nx][ny] = ".";
         }
       }
     }
   }
-  time += 1;
+}
+
+function plantBombs() {
+  if (elapsedTime <= 1) return;
+  for (let i = 0; i < R; i++) {
+    for (let j = 0; j < C; j++) {
+      if (board[i][j] === "O") continue;
+      board[i][j] = "O";
+      timerBoard[i][j] = elapsedTime + 3;
+    }
+  }
+}
+
+for (let i = 0; i < R; i++) {
+  for (let j = 0; j < C; j++) {
+    if (board[i][j] === ".") continue;
+    timerBoard[i][j] = 3;
+  }
+}
+
+for (let i = 0; i <= N; i++) {
+  // 홀수 초: 폭탄 폭발
+  if (elapsedTime % 2 === 1) {
+    explodeBombs();
+  }
+  // 짝수 초: 폭탄 설치
+  if (elapsedTime % 2 === 0) {
+    plantBombs();
+  }
+  elapsedTime += 1;
 }
 
 console.log(board.map((row) => row.join("")).join("\n"));
