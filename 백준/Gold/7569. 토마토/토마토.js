eaf163436fd4b6adc2ceae732427fb4559d67cdb
minjidev <68722909+minjidev@users.noreply.github.com>
2024-01-30 11:14:15 +0900

    [Gold V] Title: 토마토, Time: 752 ms, Memory: 145296 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.js
+++ 백준/Gold/7569. 토마토/토마토.js
@@ -23,12 +23,13 @@ class Queue {
     return Math.abs(this.head - this.tail);
   }
 }
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const [n, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
-const [M, N, H] = n.split(" ").map(Number);
+let [nums, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
+const [M, N, H] = nums.split(" ").map(Number);
 const boxes = arr.map((row) => row.split(" ").map(Number));
-
+const tomatoes = [];
 const dir = [
   [-1, 0, 0],
   [1, 0, 0],
@@ -38,63 +39,68 @@ const dir = [
   [0, 0, 1],
 ];
 
-// 3차원 배열로 만들기
-const layeredBoxes = Array.from({ length: H }, (val, idx) =>
-  boxes.slice(idx * N, idx * N + N)
-);
+for (let i = 0; i < H; i++) {
+  tomatoes.push(boxes.slice(i * N, i * N + N));
+}
 
-const q = new Queue();
+let sum = 0;
+for (let i = 0; i < H; i++) {
+  for (let j = 0; j < N; j++) {
+    for (let k = 0; k < M; k++) {
+      if (tomatoes[i][j][k] === 1) sum += 1;
+    }
+  }
+}
 
-// bfs
+if (sum === N * M * H) {
+  console.log(0);
+  return;
+}
+
 function BFS() {
-  while (!q.isEmpty()) {
-    const [h, x, y] = q.front();
-    q.pop();
+  while (!queue.isEmpty()) {
+    const [h, x, y] = queue.front();
+    queue.pop();
 
     for (let k = 0; k < 6; k++) {
       const nh = h + dir[k][0];
       const nx = x + dir[k][1];
       const ny = y + dir[k][2];
 
-      if (nx < 0 || ny < 0 || nh < 0 || nx >= N || ny >= M || nh >= H) continue;
+      if (nh < 0 || nx < 0 || ny < 0) continue;
+      if (nh >= H || nx >= N || ny >= M) continue;
 
-      if (layeredBoxes[nh][nx][ny] === 0) {
-        // 다음 칸 = 현재 칸 + 1 하여 날짜를 더한다.
-        layeredBoxes[nh][nx][ny] = layeredBoxes[h][x][y] + 1;
-        q.push([nh, nx, ny]);
+      if (tomatoes[nh][nx][ny] === 0) {
+        tomatoes[nh][nx][ny] = tomatoes[h][x][y] + 1;
+        queue.push([nh, nx, ny]);
       }
     }
   }
 }
 
-// 토마토가 익은 경우(1인 경우) 큐에 넣기
+const queue = new Queue();
 for (let i = 0; i < H; i++) {
   for (let j = 0; j < N; j++) {
     for (let k = 0; k < M; k++) {
-      if (layeredBoxes[i][j][k] === 1) {
-        q.push([i, j, k]);
-      }
+      if (tomatoes[i][j][k] === 1) queue.push([i, j, k]);
     }
   }
 }
 
 BFS();
 
-let maxVal = Number.MIN_SAFE_INTEGER;
-// 결과 확인
+let max = 0;
 for (let i = 0; i < H; i++) {
   for (let j = 0; j < N; j++) {
     for (let k = 0; k < M; k++) {
-      // 만약 익지 않은 토마토가 있다면
-      if (layeredBoxes[i][j][k] === 0) {
+      if (tomatoes[i][j][k] === 0) {
         console.log(-1);
         return;
       } else {
-        // 최댓값 구하기
-        maxVal = Math.max(maxVal, layeredBoxes[i][j][k]);
+        max = Math.max(max, tomatoes[i][j][k]);
       }
     }
   }
 }
 
-console.log(maxVal - 1);
+console.log(max - 1);
