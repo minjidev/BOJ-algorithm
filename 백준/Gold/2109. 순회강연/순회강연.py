cb613861e0917e0f10e2b1e7ab89e0b47825ce00
minjidev <68722909+minjidev@users.noreply.github.com>
2024-04-02 16:29:53 +0900

    [Gold III] Title: 순회강연, Time: 196 ms, Memory: 112920 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.py
+++ 백준/Gold/2109. 순회강연/순회강연.py
@@ -1,27 +1,16 @@
-import heapq 
+import heapq
 
 n = int(input())
 courses = [list(map(int, input().split())) for _ in range(n)]
-
-# n==0인 경우
-if n== 0:
-    print(0)
-    exit(0)
-
-# 일자 기준 오름차순
-courses.sort(key=lambda x:x[1])
-end = courses[-1][1] # 마지막 날짜
-earnings = []
 result = 0
 
-for day in range(end, 0, -1):
-    while courses and day <= courses[-1][1]:
-        heapq.heappush(earnings, -courses[-1][0])
-        courses.pop()
-        
+courses.sort(key=lambda x:x[1])
+q = []
 
-    if earnings:
-        result -= heapq.heappop(earnings)
-        
-        
-print(result)
+for p, d in courses:
+    heapq.heappush(q, p)
+
+    if len(q) > d:
+        heapq.heappop(q)
+    
+print(sum(q))
