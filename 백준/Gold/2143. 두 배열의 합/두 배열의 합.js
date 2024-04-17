a7dc6a139fbefc53b56292defb5c2d0f226092f9
minjidev <68722909+minjidev@users.noreply.github.com>
2024-04-17 11:33:23 +0900

    [Gold III] Title: 두 배열의 합, Time: 280 ms, Memory: 39052 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.js
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.js
@@ -7,67 +7,28 @@ const N = +input[0];
 const arr1 = input[1].split(" ").map(Number);
 const M = +input[2];
 const arr2 = input[3].split(" ").map(Number);
+const sumMap = new Map();
 let count = 0;
 
-// 각 배열의 누적합 저장
-const aSum = [];
-const bSum = [];
-
+// arr1의 모든 누적합을 구하고 이에 대한 개수 저장
 for (let i = 0; i < N; i++) {
   let sum = 0;
   for (let j = i; j < N; j++) {
     sum += arr1[j];
-    aSum.push(sum);
+    sumMap.set(sum, (sumMap.get(sum) || 0) + 1);
   }
 }
 
+// arr2의 모든 누적합을 구하고 sumMap 값과 더해 T를 만들 수 있는 개수를 구한다.
 for (let i = 0; i < M; i++) {
   let sum = 0;
   for (let j = i; j < M; j++) {
     sum += arr2[j];
-    bSum.push(sum);
+    const target = T - sum;
+    if (sumMap.has(target)) {
+      count += sumMap.get(target);
+    }
   }
-}
-
-aSum.sort((a, b) => a - b);
-bSum.sort((a, b) => a - b);
-
-// 이분탐색으로 B = T - A가 되는 구간 찾기
-// bisectLeft(arr, x): 정렬된 배열에서 해당 index 또는 해당 값이 들어갈 자리
-// bisectRight(arr, x): 정렬된 배열에서 해당 index + 1 또는 해당 값이 들어갈 자리
-function bisectLeft(arr, x) {
-  let s = 0;
-  let e = arr.length;
-
-  while (s < e) {
-    let mid = Math.floor((s + e) / 2);
-
-    if (arr[mid] < x) s = mid + 1;
-    else e = mid;
-  }
-
-  return s;
-}
-
-function bisectRight(arr, x) {
-  let s = 0;
-  let e = arr.length;
-
-  while (s < e) {
-    let mid = Math.floor((s + e) / 2);
-
-    if (arr[mid] <= x) s = mid + 1;
-    else e = mid;
-  }
-
-  return s;
-}
-
-for (let a of aSum) {
-  const cur = T - a;
-  const left = bisectLeft(bSum, cur);
-  const right = bisectRight(bSum, cur);
-  count += right - left;
 }
 
 console.log(count);
