07ff4a7aa8b19d8b5c1d428291e81892795413da
minjidev <68722909+minjidev@users.noreply.github.com>
2024-05-08 09:50:02 +0900

    [Gold II] Title: 보석 도둑, Time: 2152 ms, Memory: 171760 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,19 +1,26 @@
-import sys
+# 상덕이가 훔칠 수 있는 보석 가격 합의 최댓값
+# 보석, 가방 오름차순해 가벼운 보석부터 가방에 담을 수 있는만큼 담기
+
 import heapq
-input = sys.stdin.readline
+
 n, k = map(int, input().split())
 gems = [list(map(int, input().split())) for _ in range(n)]
 bags = [int(input()) for _ in range(k)]
+q = []
+result = 0
+
+
 gems.sort()
 bags.sort()
-result = 0
-values = [] # 보석 가격 저장 리스트 
 
-for bag in bags:
-    while gems and gems[0][0] <= bag: # 가방에 넣을 수 있으면 
-        heapq.heappush(values, -gems[0][1]) # 최대힙에 저장 
-        heapq.heappop(gems)
-    if values: 
-        result -= heapq.heappop(values) # 최대 가격 가져와서 더하기 
+
+for bag in bags: 
+    # 가방에 담을 수 있으면
+    while gems and gems[0][0] <= bag:
+        heapq.heappush(q, -gems[0][1]) # 보석 가격 최대힙에 담고
+        heapq.heappop(gems) # 최소 무게 보석 빼기
+
+    if q:
+        result -= heapq.heappop(q) # 최대 가격 더하기  
 
 print(result)
